ec4d666daa0cf56c7e8b51ee7397a6693db0407e
Misokira <97838368+Misokira@users.noreply.github.com>
2022-03-04 18:03:30 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -7,10 +7,24 @@ Citizen.CreateThread(function()
 		SetBlipDisplay(blip, 4)
 		SetBlipColour (blip, Config.Map[i].color)
 		SetBlipAsShortRange(blip, true)
+		SetBlipAlpha(blip, Config.Map[i].fade)
 
 		BeginTextCommandSetBlipName("STRING")
 		AddTextComponentString(Config.Map[i].name)
 		EndTextCommandSetBlipName(blip)
 	end
+	
+	for e=1, #Config.Zone, 1 do
+		
+		local blip = AddBlipForCoord(Config.Zone[e].x, Config.Zone[e].y, Config.Zone[e].z)
+		SetBlipSprite (blip, Config.Zone[e].id)
+		SetBlipDisplay(blip, 8)
+		SetBlipColour (blip, Config.Zone[e].color)
+		SetBlipAsShortRange(blip, true)
+		SetBlipPriority(blip, 0)
+		SetBlipScale(blip, 0.4)
+		SetBlipAlpha(blip, Config.Zone[e].fade)
+	end
+
 
 end)
